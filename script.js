5b4a18c52a86ff9515281c31db195ed282bf1657
Kis Levente <kis.levente@ckik.loc>
2025-03-27 14:09:41 +0100

    asd

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ label = document.querySelector("label")
 
 function StartGame(){
     GenerateCards();
+    document.querySelector("#noshowdiv").classList.add("noshowcss")
 }
 
 
@@ -18,33 +19,37 @@ function Difficulty(){
     }
 
     if(difficulty == 1){
-        cardnumber = 9
+        cardnumber = 8
     }else if(difficulty == 2){
         cardnumber = 12
     }
     else if(difficulty == 3){
-        cardnumber = 15
+        cardnumber = 14
     }
+    
     StartGame();
 }
 
-
+function randint(a, b) {
+    return Math.floor(Math.random() * (b-a+1)) + a;
+}
 
 function GenerateCards(){
     const cards = document.querySelector("#cards")
-    for(let i = 0; i < cardnumber; i++){
+    cards.innerHTML = ``;
+    let nums = (1,2,3,4,5,6,7)
+    for(let i = 0; i < cardnumber/2; i++){
+        let ran = randint(1,nums.Count())
         cards.innerHTML += `
-        <li> Kártya </li>
+        <li> <img src="Kepek/${ran}.png"></li>
+        <li> <img src="Kepek/${ran}.png"></li>
         `
     }
+
 }
 
 function MixCards(){
-
-}
-
-function NewGame(){
-
+    console.log("");
 }
 
 const button = document.querySelector("#kezdo");
